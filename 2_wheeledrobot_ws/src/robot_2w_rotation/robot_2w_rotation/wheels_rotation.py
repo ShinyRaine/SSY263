f544c144d00653e5dec211d23b6701b2d1d2e32a
xinyu <rizzlecheng@gmail.com>
2026-04-21 09:39:52 +0200

    task2 control

---
+++ 2_wheeledrobot_ws/src/robot_2w_rotation/robot_2w_rotation/wheels_rotation.py
@@ -0,0 +1,53 @@
+import rclpy
+from rclpy.node import Node
+from sensor_msgs.msg import JointState
+
+import threading
+import time
+
+class RotateWheelNode(Node):
+  def __init__(self,name):
+    super().__init__(name)
+    self.get_logger().info(f"node {name} init..")
+    self.joint_states_publisher_=self.create_publisher(
+      JointState,"joint_states", 10
+    )
+    #  the publish rate
+    self.pub_rate = self.create_rate(30) #30Hz
+    self.thread_ = threading.Thread(target=self._thread_pub)
+    self.thread_.start()
+    self._init_joint_states()
+
+  def _thread_pub(self):
+    last_update_time = time.time()
+    while rclpy.ok():
+      delta_time = time.time()-last_update_time
+      last_update_time = time.time()
+      self.joint_states.position[0] += delta_time*self.joint_states.velocity[0]
+      self.joint_states.position[1] += delta_time*self.joint_states.velocity[1]
+      self.joint_states.velocity = self.joint_speeds
+      self.joint_states.header.stamp = self.get_clock().now().to_msg()
+      self.joint_states_publisher_.publish(self.joint_states)
+      self.pub_rate.sleep()
+
+  def _init_joint_states(self):
+    self.joint_speeds =[0.0, 0.0]
+    self.joint_states = JointState()
+    self.joint_states.header.stamp=self.get_clock().now().to_msg()
+    self.joint_states.header.frame_id = ""
+    #Use the same joint names as in the URDF file
+    self.joint_states.name = ['left_wheel_joint', 'right_wheel_joint']
+    self.joint_states.position = [0.0, 0.0]
+    self.joint_states.velocity = self.joint_speeds
+    self.joint_states.effort =[]
+
+  def update_speed(self, speeds):
+    self.joint_speeds =speeds
+
+
+def main(args=None):
+  rclpy.init(args=args)
+  node = RotateWheelNode("rotate_2wRobot_wheel")
+  node.update_speed([15.0, -15.0])
+  rclpy.spin(node)
+  rclpy.shutdown()
